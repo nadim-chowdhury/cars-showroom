246156bf6b1bf37249ba971f9e5203d8bc072ec5
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-06-19 11:43:46 +0600

    show more feature added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import CarCard from "@/components/CarCard";
-import CustomFilter from "@/components/CustomFilter";
+import { fetchCars } from "@/utils";
+import { HomeProps } from "@/types";
+import { fuels, yearsOfProduction } from "@/constants";
 import Hero from "@/components/Hero";
 import SearchBar from "@/components/SearchBar";
-import { fetchCars } from "@/utils";
+import CustomFilter from "@/components/CustomFilter";
+import CarCard from "@/components/CarCard";
+import ShowMore from "@/components/ShowMore";
 
-export default async function Home({ searchParams }) {
+export default async function Home({ searchParams }: HomeProps) {
   const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer,
+    manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
     fuel: searchParams.fuel || "",
     limit: searchParams.limit || 10,
@@ -19,30 +22,37 @@ export default async function Home({ searchParams }) {
     <main className="overflow-hidden">
       <Hero />
 
-      <div className="mt-10 padding-y" id="discover">
-        <div className="home__text-container text-center">
-          <h3 className="text-3xl font-bold">Car Catalouge</h3>
-          <p>Explore the cars You might like</p>
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
         </div>
 
         <div className="home__filters">
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
         {!isDataEmpty ? (
-          <div className="home__cars-wrapper">
-            {allCars?.map((car) => (
-              <CarCard car={car} />
-            ))}
-          </div>
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+          </section>
         ) : (
           <div className="home__error-container">
-            <h2 className="text-xl text-black font-bold">Oopd, no results</h2>
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
--- types/index.ts
+++ types/index.ts
@@ -1,18 +1,50 @@
 import { MouseEventHandler } from "react";
 
-export interface CustomButtonProps {
-  title: string;
-  containerStyles?: string;
-  handleClick?: MouseEventHandler<HTMLButtonElement>;
-  btnType?: "button" | "submit";
-  textStyles?: string;
-  rightIcon?: string;
-  isDisabled?: boolean;
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
 
-export interface SearchManufacturerProps {
-  manufacturer: string;
-  setManufacturer: (manufacturer: string) => void;
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
+
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+
+export interface CarCardProps {
+  model: string;
+  make: string;
+  mpg: number;
+  transmission: string;
+  year: number;
+  drive: string;
+  cityMPG: number;
+}
+
+export interface CustomButtonProps {
+  isDisabled?: boolean;
+  btnType?: "button" | "submit";
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface OptionProps {
@@ -25,25 +57,12 @@ export interface CustomFilterProps {
   options: OptionProps[];
 }
 
-export interface CarProps {
-  city_mpg: number;
-  class: string;
-  combination_mpg: number;
-  cylinders: number;
-  displacement: number;
-  drive: string;
-  fuel_type: "gas";
-  highway_mpg: number;
-  make: string;
-  model: string;
-  transmission: string;
-  year: number;
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
 }
 
-export interface FilterProps {
+export interface SearchManuFacturerProps {
   manufacturer: string;
-  year: number;
-  fuel: string;
-  limit: number;
-  model: string;
+  setManuFacturer: (manufacturer: string) => void;
 }
